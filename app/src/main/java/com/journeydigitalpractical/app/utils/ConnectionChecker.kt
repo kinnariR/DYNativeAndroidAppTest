6b434b5e3e647a7e7dbef6de2137b73bed6489d8
kinnari.pandya <kinnari.pandya@radixweb.com>
2022-05-17 17:48:26 +0530

    Network configuration changes

---
+++ app/src/main/java/com/journeydigitalpractical/app/utils/ConnectionChecker.kt
@@ -0,0 +1,28 @@
+package com.journeydigitalpractical.app.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+/**
+ * Helper class for internet connectivity
+ */
+object ConnectionChecker {
+    fun checkConnection(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+            return nwInfo.isConnected
+        }
+    }
+}
